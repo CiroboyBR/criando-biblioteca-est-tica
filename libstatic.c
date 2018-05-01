f0f94ba4aa19fcd7deefd5ad33a204835d357694
Ciro <CiroboyBR@users.noreply.github.com>
2018-05-01 10:53:42 -0300

    Add files via upload

---
+++ libstatic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//soma
+int somaNum(int a, int b)
+{
+    return a + b;
+}
+
+//quadrado
+long int quadradoNum(int n)
+{
+    return n * n;
+}
+
+//imprime
+void imprimeOla()
+{
+    printf("Ola!");
+
+}
---
+++ programa.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%d\n", somaNum(2, 2));
+    printf("%li\n", quadradoNum(6));
+
+    imprimeOla();
+
+
+  return 0;
+}
